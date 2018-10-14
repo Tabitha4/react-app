eefff3367c42bd05997f30057230ccaed0b1b4f8
Tabitha4 <tabithalevine@hotmail.co>
2018-10-14 10:05:34 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
+    const array = ["React ~ ", "Javascript ~ ", "ES6 ~ ", "Redux "];
+
+    var arrayToRender = [];
+    array.forEach(function(array) {
+      arrayToRender.push(array);
+    });
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+          <p className="myArray">{arrayToRender}</p>
+          <p style={{ fontSize: "80px" }} className="text-large">
+            Hiya!
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+          <p style={{ fontSize: "40px" }} className="learnParagraph">
+            This is me trying to learn some React
+          </p>
+          <p className="youLearn">
+            If you want to learn it also, click{" "}
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              here
+            </a>
+          </p>
+          <button
+            onClick={() => {
+              console.log("Hey, this was clicked!");
+            }}
           >
-            Learn React
-          </a>
+            Press ME to test to the console
+          </button>
         </header>
       </div>
     );
